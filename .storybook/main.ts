fc6ed20e28129603f373743e7c3ace7706ec8304
Rishabh Anand <ranand16@gmail.com>
2023-08-06 12:43:57 +0530

    Initial commit

---
+++ .storybook/main.ts
@@ -0,0 +1,25 @@
+import type { StorybookConfig } from "@storybook/react-webpack5";
+
+const config: StorybookConfig = {
+    stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/preset-create-react-app",
+        "@storybook/addon-onboarding",
+        "@storybook/addon-interactions",
+    ],
+    framework: {
+        name: "@storybook/react-webpack5",
+        options: {},
+    },
+    docs: {
+        autodocs: "tag",
+    },
+    staticDirs: ["../public"],
+    features: {
+        legacyMdx1: true, // 👈 Enables MDX v1 support
+        storyStoreV7: false, // 👈 Opt out of on-demand story loading
+    },
+};
+export default config;
---
+++ src/Components/TextButton/TextButton.tsx
@@ -0,0 +1,10 @@
+import Button from "@mui/material/Button";
+type ButtonProps = { label: string };
+function TextButton({ label }: ButtonProps) {
+    return (
+        <>
+            <Button variant="contained">{label}</Button>{" "}
+        </>
+    );
+}
+export default TextButton;
