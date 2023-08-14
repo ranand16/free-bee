760258d337b30a9fae81ae6cac5f7f4fa0d26957
Rishabh Anand <ranand16@gmail.com>
2023-08-14 17:00:03 +0530

    Added react to stories as well for the previous error

--- src/Stories/RatingStar.stories.tsx
+++ src/Stories/RatingStar.stories.tsx
@@ -1,13 +1,14 @@
+import * as React from "react";
 import RatingStar from "../Components/RatingStar";
 import { Meta } from "@storybook/react";
 
 export default {
-  title: "Components/Rating",
-  component: RatingStar,
-  tags: ["autodocs"],
-  argTypes: {
-    backgroundColor: { control: "color" },
-  },
+    title: "Components/Rating",
+    component: RatingStar,
+    tags: ["autodocs"],
+    argTypes: {
+        backgroundColor: { control: "color" },
+    },
 } as Meta<typeof RatingStar>;
 
 export const Default = () => <RatingStar />;
