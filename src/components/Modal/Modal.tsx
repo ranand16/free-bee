dc7ded24e4d957134bf96cddbe17e9745cf5b406
Rishabh Anand <ranand16@gmail.com>
2023-09-19 20:46:32 +0530

    Added modal footer header and body default classes

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -7,7 +7,7 @@ interface ModalProps extends React.HTMLAttributes<HTMLDivElement> {}
 const Modal: FC<ModalProps> = () => {
     return (
         <BlanketComponent>
-            <ModalContent className="bg-slate-700" />
+            <ModalContent className="bg-slate-700 shadow-modal" />
         </BlanketComponent>
     );
 };
