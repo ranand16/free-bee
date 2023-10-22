1de15bdffdfad790b9a7dddd72c9d700617c75cf
Rishabh Anand <ranand16@gmail.com>
2023-10-22 17:10:04 +0530

    dark mode toggle done

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -4,43 +4,48 @@ import { cva, VariantProps } from "class-variance-authority";
 import { cn } from "../../utility/functions";
 
 const TagVariants = cva("", {
-  variants: {
-    variant: {
-      default: "",
+    variants: {
+        variant: {
+            default: "",
+        },
     },
-  },
-  defaultVariants: {
-    variant: "default",
-  },
+    defaultVariants: {
+        variant: "default",
+    },
 });
 
 interface InputFieldProps
-  extends InputHTMLAttributes<HTMLInputElement>,
-    VariantProps<typeof TagVariants> {
-  label?: string;
-  href?: string;
-  removeButtonLabel?: any;
+    extends InputHTMLAttributes<HTMLInputElement>,
+        VariantProps<typeof TagVariants> {
+    label?: string;
+    href?: string;
+    removeButtonLabel?: any;
 }
 
 const Tag = forwardRef<HTMLInputElement, InputFieldProps>(
-  ({ label, href, className, variant, removeButtonLabel, ...props }, ref) => {
-    return (
-      <div
-        className={`tag inline-flex items-center h-[20px] m-[4px] p-0 relative bg-default-bold dark:bg-inverse ${href ? "dark:hover:bg-neutral-hovered" : ""} rounded-[3px] overflow-hidden`}
-        ref={ref}
-      >
-        {href ? (
-          <a href={href} className="text-sm whitespace-nowrap text-ellipsis max-w-[180px] px-[4px] py-[2px] no-underline hover:underline text-brand-bold hover:bg-default-bold-hovered">
-            {label}
-          </a>
-        ) : (
-          <span className="text-sm whitespace-nowrap text-ellipsis dark:text-neutral-bold-hovered max-w-[180px] px-[4px] py-[2px]">
-            {label}
-          </span>
-        )}
-      </div>
-    );
-  }
+    ({ label, href, className, variant, removeButtonLabel, ...props }, ref) => {
+        return (
+            <div
+                className={`tag inline-flex items-center h-[20px] m-[4px] p-0 relative bg-default-bold dark:bg-inverse ${
+                    href ? "dark:hover:bg-neutral-hovered" : ""
+                } rounded-[3px] overflow-hidden`}
+                ref={ref}
+            >
+                {href ? (
+                    <a
+                        href={href}
+                        className="text-sm whitespace-nowrap text-ellipsis max-w-[180px] px-[4px] py-[2px] no-underline hover:underline text-brand-bold hover:bg-default-bold-hovered"
+                    >
+                        {label}
+                    </a>
+                ) : (
+                    <span className="text-sm whitespace-nowrap text-ellipsis dark:text-neutral-bold-hovered-dark max-w-[180px] px-[4px] py-[2px]">
+                        {label}
+                    </span>
+                )}
+            </div>
+        );
+    }
 );
 
 export default Tag;
--- src/components/Toggle/Toggle.tsx
+++ src/components/Toggle/Toggle.tsx
@@ -16,7 +16,7 @@ const Button = forwardRef<HTMLLabelElement, ToggleProps>(
                 ref={ref}
                 data-checked={checked}
                 className={cn(
-                    "hover:bg-neutral-bold-hovered hover:data-[checked=true]:bg-success-bold-hovered focus-within:border-focus-dark data-[checked=true]:bg-accent-green-bolder before:transform before:duration-200 before:ease-in-out   data-[checked=true]:before:text-inverse data-[checked=true]:before:translate-x-4 data-[checked=true]:text-inverse cursor-pointer box-content inline-block p-[2px] m-[2px] bg-neutral-bold rounded-2xl border-2 border-transparent h-4 relative w-8 text-inverse before:bg-inverse before:rounded-full before:contents-[''] before:absolute before:bottom-1 before:h-3 before:w-3 before:left-1",
+                    "dark:bg-neutral-bold dark:hover:bg-neutral-bold-hovered-dark hover:bg-neutral-bold-hovered dark:hover:data-[checked=true]:bg-success-bold-hovered hover:data-[checked=true]:bg-success-pressed dark:focus-within:border-focus-dark focus-within:border-focus-dark dark:data-[checked=true]:bg-accent-green-bolder data-[checked=true]:bg-success-bold before:transform before:duration-200 before:ease-in-out data-[checked=true]:before:text-white dark:data-[checked=true]:before:text-inverse data-[checked=true]:before:translate-x-4 dark:data-[checked=true]:text-inverse data-[checked=true]:text-white cursor-pointer box-content inline-block p-[2px] m-[2px] bg-subtle rounded-2xl border-2 border-transparent h-4 relative w-8 text-white dark:text-inverse before:bg-white dark:before:bg-inverse before:rounded-full before:contents-[''] before:absolute before:bottom-1 before:h-3 before:w-3 before:left-1",
                     className
                 )}
                 id={id}
@@ -36,7 +36,7 @@ const Button = forwardRef<HTMLLabelElement, ToggleProps>(
                         height="24"
                         viewBox="0 0 24 24"
                         role="presentation"
-                        className="overflow-hidden pointer-events-none max-w-full max-h-full fill-surface-primary text-surface-primary align-bottom"
+                        className="overflow-hidden pointer-events-none max-w-full max-h-full dark:fill-surface-primary  fill-white text-white dark:text-surface-primary align-bottom"
                     >
                         <path
                             d="M7.356 10.942a.497.497 0 00-.713 0l-.7.701a.501.501 0 00-.003.71l3.706 3.707a.501.501 0 00.705.003l7.712-7.712a.493.493 0 00-.006-.708l-.7-.7a.504.504 0 00-.714 0l-6.286 6.286a.506.506 0 01-.713 0l-2.288-2.287z"
@@ -53,7 +53,7 @@ const Button = forwardRef<HTMLLabelElement, ToggleProps>(
                         height="24"
                         viewBox="0 0 24 24"
                         role="presentation"
-                        className="overflow-hidden pointer-events-none max-w-full max-h-full fill-surface-primary text-surface-primary align-bottom"
+                        className="overflow-hidden pointer-events-none max-w-full max-h-full dark:fill-surface-primary  fill-white text-white dark:text-surface-primary align-bottom"
                     >
                         <path
                             d="M15.185 7.4l-3.184 3.185-3.186-3.186a.507.507 0 00-.712.003l-.7.701a.496.496 0 00-.004.712l3.185 3.184L7.4 15.185a.507.507 0 00.004.712l.7.7c.206.207.516.2.712.004l3.186-3.185 3.184 3.185a.507.507 0 00.712-.004l.701-.7a.496.496 0 00.003-.712l-3.186-3.186 3.186-3.184a.507.507 0 00-.003-.712l-.7-.7a.508.508 0 00-.36-.153.5.5 0 00-.353.15z"
