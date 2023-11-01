51fa1c08ed637f4fdaf52d20f13fd449bddfd260
Rishabh Anand <ranand16@gmail.com>
2023-11-01 22:46:21 +0530

    added tab

---
+++ src/components/Tab/Tab.stories.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { Meta } from "@storybook/react";
+import Tag from "./Tab";
+import AccessibilityIcon from "@mui/icons-material/Accessibility";
+
+export default {
+    title: "Components/Tag",
+    component: Tag,
+    tags: ["autodocs"],
+    argTypes: {},
+} as Meta<typeof Tag>;
+
+export const Standard = () => <Tag label="Tag" />;
+
+export const LinkTag = () => <Tag label="Tag Link" href="/" />;
