4b08c59f359556e5b9b0e3430d1e86aa0d023673
Rishabh Anand <ranand16@gmail.com>
2023-11-02 22:06:38 +0530

    added tab componnets

---
+++ src/components/Tab/Tabs.tsx
@@ -0,0 +1,51 @@
+import * as React from "react";
+import { HTMLAttributes, ChangeEventHandler, forwardRef } from "react";
+import { cva, VariantProps } from "class-variance-authority";
+import { cn } from "../../utility/functions";
+
+const TabsVariants = cva("", {
+    variants: {
+        variant: {
+            default: "",
+        },
+    },
+    defaultVariants: {
+        variant: "default",
+    },
+});
+
+export interface TabsProps
+    extends HTMLAttributes<HTMLDivElement>,
+        VariantProps<typeof TabsVariants> {
+    onTabChange: (selected: number) => void;
+}
+
+const Tabs = forwardRef<HTMLDivElement, TabsProps>(
+    ({ id, onTabChange, children }, ref) => {
+        const [selected, setSelected] = React.useState(0);
+
+        React.useEffect(() => {
+            console.log("tab item :: ");
+            onTabChange(selected);
+        }, [selected]);
+
+        const tabClick = (e: any) => {
+            if (ref && ref?.current) {
+                console.log(
+                    "ref current ",
+                    document
+                        .getElementById(id as string)
+                        ?.getElementsByClassName("tab-item")
+                );
+                console.log("ref current ", e.target);
+            }
+        };
+        return (
+            <div id={id} ref={ref} className={""} onClick={tabClick}>
+                {children}
+            </div>
+        );
+    }
+);
+
+export default Tabs;
