08194c258aefca72e38b07ac673a717bb76dbd5d
Rishabh Anand <ranand16@gmail.com>
2023-12-05 21:17:27 +0530

    dropdowniteam added and code refactor

--- src/components/Dropdown Menu/DropdownMenu.stories.tsx
+++ src/components/Dropdown Menu/DropdownMenu.stories.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
 import DropdownMenu from "./DropdownMenu";
 import { Meta } from "@storybook/react";
-import DropdownMenuItems from "./DropdownMenuItems";
+import DropdownMenuItemGroup from "./DropdownMenuItemGroup";
+import DropdownMenuItem from "./DropdownMenuItem";
 
 export default {
     title: "Components/DropdownMenu",
@@ -49,7 +50,99 @@ export const DropdownMenuComponent = () => {
                 callback={toggleNow}
                 label={"Page actions"}
             >
-                <DropdownMenuItems ref={dropdownMenuItemsRef} />
+                <DropdownMenuItemGroup ref={dropdownMenuItemsRef}>
+                    <DropdownMenuItem label="Edit" />
+                    <DropdownMenuItem label="Share" />
+                    <DropdownMenuItem label="Move" />
+                    <DropdownMenuItem label="Clone" />
+                    <DropdownMenuItem label="Share" />
+                    <DropdownMenuItem label="Delete" />
+                    <DropdownMenuItem label="Report" />
+                    <button
+                        role="menuitem"
+                        className="relative flex box-border w-full min-h-[40px] m-0 items-center text-sm outline-[0px] no-underline select-none py-2 px-4 cursor-pointer dark:text-white hover:bg-default-bold dark:hover:text-white dark:hover:bg-neutral-subtle-hovered"
+                        type="button"
+                    >
+                        <span className="">
+                            <span className="">
+                                <span className="" data-item-title="true">
+                                    Edit
+                                </span>
+                            </span>
+                        </span>
+                    </button>
+                </DropdownMenuItemGroup>
+            </DropdownMenu>
+        </div>
+    );
+};
+
+export const DropdownMenuComponentButtonDescription = () => {
+    const [selected, setSelected] = React.useState(false);
+    const [open, setOpen] = React.useState(false);
+    const dropdownMenuItemsRef = React.useRef<any>(null);
+    const btnRef = React.useRef<any>(null);
+
+    const toggleNow = React.useCallback((e: any) => {
+        setSelected((prevState) => !prevState);
+        setOpen((prevState) => !prevState);
+    }, []);
+
+    const handler = (event: MouseEvent) => {
+        if (!dropdownMenuItemsRef.current) return;
+        if (
+            !dropdownMenuItemsRef.current.contains(event.target) &&
+            !btnRef.current.contains(event.target)
+        ) {
+            setSelected(false);
+            setOpen(false);
+        }
+    };
+
+    React.useEffect(() => {
+        // the key is using the `true` option
+        // `true` will enable the `capture` phase of event handling by browser
+        document.addEventListener("click", handler, true);
+        return () => {
+            document.removeEventListener("click", handler, true);
+        };
+    }, []);
+
+    return (
+        <div className="relative">
+            {/** This needs to have a height while use */}
+            <DropdownMenu
+                ref={btnRef}
+                open={open}
+                selected={selected}
+                callback={toggleNow}
+                label={"Page actions"}
+            >
+                <DropdownMenuItemGroup ref={dropdownMenuItemsRef}>
+                    <DropdownMenuItem
+                        label="Edit"
+                        description="Previous edits were saved"
+                    />
+                    <DropdownMenuItem label="Share" />
+                    <DropdownMenuItem label="Move" />
+                    <DropdownMenuItem label="Clone" />
+                    <DropdownMenuItem label="Share" />
+                    <DropdownMenuItem label="Delete" />
+                    <DropdownMenuItem label="Report" />
+                    <button
+                        role="menuitem"
+                        className="relative flex box-border w-full min-h-[40px] m-0 items-center text-sm outline-[0px] no-underline select-none py-2 px-4 cursor-pointer dark:text-white hover:bg-default-bold dark:hover:text-white dark:hover:bg-neutral-subtle-hovered"
+                        type="button"
+                    >
+                        <span className="">
+                            <span className="">
+                                <span className="" data-item-title="true">
+                                    Edit
+                                </span>
+                            </span>
+                        </span>
+                    </button>
+                </DropdownMenuItemGroup>
             </DropdownMenu>
         </div>
     );
---
+++ src/components/Dropdown Menu/DropdownMenuItemGroup.tsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+import DropdownMenuItem from "./DropdownMenuItem";
+
+export interface DropdownMenuItemGroupProps
+    extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const DropdownMenuItemGroup = React.forwardRef<
+    HTMLDivElement,
+    DropdownMenuItemGroupProps
+>(({ children }, ref) => {
+    return (
+        <div
+            id=""
+            ref={ref}
+            data-placement="bottom-start"
+            tabIndex={0}
+            className="fixed block box-border my-0 p-0 ml-3 mt-12 bg-white dark:bg-overlay shadow-light-box overflow-auto z-[400]"
+            style={{
+                inset: "0px auto auto 0px",
+            }}
+        >
+            <div
+                aria-busy="false"
+                role="menu"
+                className="max-w-screen-md flex static flex-col overflow-auto"
+                style={{
+                    maxHeight: "calc(100vh - 16px)",
+                    boxSizing: "inherit",
+                }}
+            >
+                <div
+                    className="shrink-0 m-0 p-0 block before:block before:h-[6px] before:content-[''] after:block after:h-[6px] after:content-['']"
+                    role="group"
+                    data-section="true"
+                    style={{ boxSizing: "inherit" }}
+                >
+                    {children}
+                </div>
+            </div>
+        </div>
+    );
+});
+
+export default DropdownMenuItemGroup;
