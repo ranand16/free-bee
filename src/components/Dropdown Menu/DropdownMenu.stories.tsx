9fd60508c9e556eaff91e49deba972fdf270447a
Rishabh Anand <ranand16@gmail.com>
2023-12-07 14:19:00 +0530

    dropdown item disbaled with description light and adark them

--- src/components/Dropdown Menu/DropdownMenu.stories.tsx
+++ src/components/Dropdown Menu/DropdownMenu.stories.tsx
@@ -147,3 +147,195 @@ export const DropdownMenuComponentButtonDescription = () => {
         </div>
     );
 };
+
+export const DropdownMenuComponentButtonTitleWrap = () => {
+    const [selected, setSelected] = React.useState(false);
+    const [open, setOpen] = React.useState(false);
+    const dropdownMenuItemsRef = React.useRef<any>(null);
+    const btnRef = React.useRef<any>(null);
+
+    const toggleNow = React.useCallback((e: any) => {
+        setSelected((prevState) => !prevState);
+        setOpen((prevState) => !prevState);
+    }, []);
+
+    const handler = (event: MouseEvent) => {
+        if (!dropdownMenuItemsRef.current) return;
+        if (
+            !dropdownMenuItemsRef.current.contains(event.target) &&
+            !btnRef.current.contains(event.target)
+        ) {
+            setSelected(false);
+            setOpen(false);
+        }
+    };
+
+    React.useEffect(() => {
+        // the key is using the `true` option
+        // `true` will enable the `capture` phase of event handling by browser
+        document.addEventListener("click", handler, true);
+        return () => {
+            document.removeEventListener("click", handler, true);
+        };
+    }, []);
+
+    return (
+        <div className="relative">
+            {/** This needs to have a height while use */}
+            <DropdownMenu
+                ref={btnRef}
+                open={open}
+                selected={selected}
+                callback={toggleNow}
+                label={"Page actions"}
+            >
+                <DropdownMenuItemGroup ref={dropdownMenuItemsRef}>
+                    <DropdownMenuItem
+                        label="Edit"
+                        description="Previous edits were saved"
+                    />
+                    <DropdownMenuItem
+                        labelWrap
+                        label="Lorem ipsum: In quis ligula ut massa scelerisque cursus. Nulla et dolor felis. Mauris euismod neque at venenatis tempor."
+                    />
+                    <DropdownMenuItem label="Lorem ipsum: In quis ligula ut massa scelerisque cursus. Nulla et dolor felis. Mauris euismod neque at venenatis tempor." />
+                    <DropdownMenuItem label="Lorem ipsum: In quis ligula ut massa scelerisque cursus. Nulla et dolor felis. Mauris euismod neque at venenatis tempor." />
+                </DropdownMenuItemGroup>
+            </DropdownMenu>
+        </div>
+    );
+};
+
+export const DropdownMenuComponentButtonDescriptionWrap = () => {
+    const [selected, setSelected] = React.useState(false);
+    const [open, setOpen] = React.useState(false);
+    const dropdownMenuItemsRef = React.useRef<any>(null);
+    const btnRef = React.useRef<any>(null);
+
+    const toggleNow = React.useCallback((e: any) => {
+        setSelected((prevState) => !prevState);
+        setOpen((prevState) => !prevState);
+    }, []);
+
+    const handler = (event: MouseEvent) => {
+        if (!dropdownMenuItemsRef.current) return;
+        if (
+            !dropdownMenuItemsRef.current.contains(event.target) &&
+            !btnRef.current.contains(event.target)
+        ) {
+            setSelected(false);
+            setOpen(false);
+        }
+    };
+
+    React.useEffect(() => {
+        // the key is using the `true` option
+        // `true` will enable the `capture` phase of event handling by browser
+        document.addEventListener("click", handler, true);
+        return () => {
+            document.removeEventListener("click", handler, true);
+        };
+    }, []);
+
+    return (
+        <div className="relative">
+            {/** This needs to have a height while use */}
+            <DropdownMenu
+                ref={btnRef}
+                open={open}
+                selected={selected}
+                callback={toggleNow}
+                label={"Page actions"}
+            >
+                <DropdownMenuItemGroup ref={dropdownMenuItemsRef}>
+                    <DropdownMenuItem
+                        label="Edit"
+                        description="Previous edits were saved"
+                    />
+                    <DropdownMenuItem
+                        descriptionWrap
+                        label="Lorem ipsum: In quis ligula ut massa scelerisque cursus. Nulla et dolor felis. Mauris euismod neque at venenatis tempor."
+                        description="Previous edits were saved. This line will be so long that it needs to wrap to next line and tou should be seeing somepart of this sentence wrapped."
+                    />
+                    <DropdownMenuItem
+                        description="Previous edits were saved. This line will be so long that it needs to wrap to next line and tou should be seeing somepart of this sentence wrapped."
+                        label="Lorem ipsum: In quis ligula ut massa scelerisque cursus. Nulla et dolor felis. Mauris euismod neque at venenatis tempor."
+                    />
+                    <DropdownMenuItem label="Lorem ipsum: In quis ligula ut massa scelerisque cursus. Nulla et dolor felis. Mauris euismod neque at venenatis tempor." />
+                </DropdownMenuItemGroup>
+            </DropdownMenu>
+        </div>
+    );
+};
+
+export const DropdownMenuComponentButtonDisabled = () => {
+    const [selected, setSelected] = React.useState(false);
+    const [open, setOpen] = React.useState(false);
+    const dropdownMenuItemsRef = React.useRef<any>(null);
+    const btnRef = React.useRef<any>(null);
+
+    const toggleNow = React.useCallback((e: any) => {
+        setSelected((prevState) => !prevState);
+        setOpen((prevState) => !prevState);
+    }, []);
+
+    const handler = (event: MouseEvent) => {
+        if (!dropdownMenuItemsRef.current) return;
+        if (
+            !dropdownMenuItemsRef.current.contains(event.target) &&
+            !btnRef.current.contains(event.target)
+        ) {
+            setSelected(false);
+            setOpen(false);
+        }
+    };
+
+    React.useEffect(() => {
+        // the key is using the `true` option
+        // `true` will enable the `capture` phase of event handling by browser
+        document.addEventListener("click", handler, true);
+        return () => {
+            document.removeEventListener("click", handler, true);
+        };
+    }, []);
+
+    return (
+        <div className="relative">
+            {/** This needs to have a height while use */}
+            <DropdownMenu
+                ref={btnRef}
+                open={open}
+                selected={selected}
+                callback={toggleNow}
+                label={"Page actions"}
+            >
+                <DropdownMenuItemGroup ref={dropdownMenuItemsRef}>
+                    <DropdownMenuItem
+                        isDisabled
+                        label="Edit"
+                        description="Previous edits were saved"
+                    />
+                    <DropdownMenuItem label="Share" />
+                    <DropdownMenuItem isDisabled label="Move" />
+                    <DropdownMenuItem label="Clone" />
+                    <DropdownMenuItem label="Share" />
+                    <DropdownMenuItem label="Delete" />
+                    <DropdownMenuItem label="Report" />
+                    <button
+                        role="menuitem"
+                        className="relative flex box-border w-full min-h-[40px] m-0 items-center text-sm outline-[0px] no-underline select-none py-2 px-4 cursor-pointer dark:text-white hover:bg-default-bold dark:hover:text-white dark:hover:bg-neutral-subtle-hovered"
+                        type="button"
+                    >
+                        <span className="">
+                            <span className="">
+                                <span className="" data-item-title="true">
+                                    Edit
+                                </span>
+                            </span>
+                        </span>
+                    </button>
+                </DropdownMenuItemGroup>
+            </DropdownMenu>
+        </div>
+    );
+};
--- src/components/Dropdown Menu/DropdownMenuItem.tsx
+++ src/components/Dropdown Menu/DropdownMenuItem.tsx
@@ -1,32 +1,68 @@
 import * as React from "react";
+import { cn } from "../../utility/functions";
 
 export interface DropdownMenuItemProps
     extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     label: string;
     description?: string;
+    labelWrap?: boolean;
+    descriptionWrap?: boolean;
+    isDisabled?: boolean;
 }
 
 const DropdownMenuItem: React.FC<DropdownMenuItemProps> = ({
     label,
     description,
+    labelWrap = false,
+    descriptionWrap = false,
+    isDisabled = false,
     children,
 }) => {
     return (
         <button
             role="menuitem"
-            className="relative flex box-border w-full min-h-[40px] m-0 items-center text-sm outline-[0px] no-underline select-none py-2 px-4 cursor-pointer dark:text-white hover:bg-default-bold dark:hover:text-white dark:hover:bg-neutral-subtle-hovered"
+            className={cn(
+                "relative flex box-border w-full min-h-[40px] m-0 items-center text-sm outline-[0px] no-underline select-none py-2 px-4 cursor-pointer dark:text-white hover:bg-default-bold dark:hover:text-white dark:hover:bg-neutral-subtle-hovered",
+                {
+                    "cursor-not-allowed text-disabled-alternate hover:text-disabled-alternate dark:text-disabled-txt dark:hover:text-disabled-txt bg-neutral-subtle hover:bg-neutral-subtle dark:bg-neutral-subtle dark:hover:bg-neutral-subtle":
+                        isDisabled,
+                }
+            )}
             type="button"
         >
             {children}
             {!children && (
-                <span className="">
+                <span className="flex box-border gap-3 items-center justify-center w-full flex-grow">
                     <span className="flex justify-center flex-col grow leading-4 text-left overflow-hidden">
-                        <span className="" data-item-title="true">
+                        <span
+                            className={cn(
+                                {
+                                    "text-disabled-alternate hover:text-disabled-alternate dark:text-disabled-txt dark:hover:text-disabled-txt flex flex-col justify-center flex-grow leading-4 break-words":
+                                        isDisabled,
+                                },
+                                {
+                                    "block overflow-hidden text-ellipsis whitespace-nowrap":
+                                        labelWrap,
+                                }
+                            )}
+                            data-item-title="true"
+                        >
                             {label}
                         </span>
-                        <span className="mt-1 text-xs dark:text-subtlest-old text-subtlest-old break-words">
-                            {description}
-                        </span>
+                        {description && (
+                            <span
+                                className={cn(
+                                    "mt-1 text-xs dark:text-subtlest-old text-subtlest-old ",
+                                    { "break-words": !descriptionWrap },
+                                    {
+                                        "block overflow-hidden text-ellipsis whitespace-nowrap":
+                                            descriptionWrap,
+                                    }
+                                )}
+                            >
+                                {description}
+                            </span>
+                        )}
                     </span>
                 </span>
             )}
